5649429c8d3be161c90e6310f45de6e9f65f5c1d
damiancodes <damianbwire88@gmail.com>
2025-05-28 20:03:15 +0300

    commit

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -5,6 +5,7 @@ import swal from 'sweetalert';
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 import homepageImage from '../images/homepage5.jpg';
 
@@ -14,7 +15,12 @@ function Login() {
   const navigate = useNavigate();
   const [signInBtn, showsignInBtn] = useState(true);
   const [loadingBtn, showloadingBtn] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   function signInUser() {
     const emailInput = email.current.value;
     const passInput = password.current.value;
@@ -112,13 +118,35 @@ function Login() {
           
           <Form.Group className="mb-4" controlId="loginPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control 
-              type="password" 
-              placeholder="password" 
-              ref={password}
-              required
-              className="modern-input"
-            />
+            <InputGroup>
+              <Form.Control 
+                type={showPassword ? "text" : "password"}
+                placeholder="password" 
+                ref={password}
+                required
+                className="modern-input"
+              />
+              <Button 
+                variant="outline-secondary" 
+                onClick={togglePasswordVisibility}
+                className="password-toggle-btn"
+                type="button"
+                aria-label={showPassword ? "Hide password" : "Show password"}
+              >
+                {showPassword ? (
+                  <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/>
+                    <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/>
+                    <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.708zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>
+                  </svg>
+                ) : (
+                  <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                  </svg>
+                )}
+              </Button>
+            </InputGroup>
           </Form.Group>
           
           {signInBtn && 
